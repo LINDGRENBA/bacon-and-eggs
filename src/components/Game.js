51fd78f5324b4997b3e3ef4c67ab3e9d013296e4
Brittany <brittdevelopsit@gmail.com>
2021-04-25 16:55:59 -0700

    Add three main components

---
+++ src/components/Game.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Board from './Board';
+import { checkForWinner } from './../calculate.js';
+
+const Game = () => {
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [baconIsNext, setBaconIsNext] = useState(true);
+    const winner = checkForWinner(history[stepNumber]);
+    const baconEggs = baconIsNext ? "bacon" : "eggs";
+
+    const handleClick = (i) => {
+        const historyPoint = history.slice(0, stepNumber+1);
+        const current = historyPoint[stepNumber];
+        const squares = [...current];
+
+        if(winner || squares[i]) return;
+        squares[i] = baconEggs;
+
+        setHistory([...historyPoint, squares]);
+        setStepNumber(historyPoint.length);
+        setBaconIsNext(!baconIsNext);
+    }
+
+
+    return (
+        <React.Fragment>
+            <h1>Bacon and Eggs - Tic Tac Toe</h1>
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <h3>{ winner ? "Winner: " + winner : "Next Player: " + baconEggs }</h3>
+        </React.Fragment>
+    )
+
+};
+
+export default Game;
